a6e0c7f3ede1949f5170c272700766fdfd8638ea
Рустам <rustam19871203@gmail.com>
2022-09-30 18:30:30 +0400

    Переменные 2 Задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int number_1 = 1;
+        System.out.println("Значение переменной number_1 с типом int равно " + number_1);
+        byte number_2 = 2;
+        System.out.println("Значение переменной number_2 с типом byte равно " + number_2);
+        short number_3 = 3;
+        System.out.println("Значение переменной number_3 с типом short равно " + number_3);
+        long number_4 = 4;
+        System.out.println("Значение переменной number_4 с типом long равно " + number_4);
+        float number_5 = 5;
+        System.out.println("Значение переменной number_5 с типом float равно " + number_5);
+        double number_6 = 6;
+        System.out.println("Значение переменной number_6 с типом double равно " + number_6);
+    }
+}
